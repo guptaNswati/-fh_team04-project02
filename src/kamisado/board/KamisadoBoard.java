617624357b6d61b61fc29a80eeb3682f31205420
HanyuChen0121 <mattmelody0121@gmail.com>
2016-06-07 18:32:47 -0700

    Add files via upload

--- src/kamisado/board/KamisadoBoard.java
+++ src/kamisado/board/KamisadoBoard.java
@@ -29,52 +29,69 @@ public class KamisadoBoard extends JPanel
             panel.add(boardPiece.squares[x][y], gbc);
          }
       ImageIcon BlackOrange = new ImageIcon(new ImageIcon("BlackOrange.png").getImage().getScaledInstance(45, 45, Image.SCALE_DEFAULT));
-      boardPiece.squares[0][0].add(new JLabel(BlackOrange));
+      JLabel LableBlackOrange = new JLabel(null, BlackOrange, JLabel.CENTER);
+      boardPiece.squares[0][1].add(LableBlackOrange);
       
       ImageIcon BlackBlue = new ImageIcon(new ImageIcon("BlackBlue.png").getImage().getScaledInstance(45, 45, Image.SCALE_DEFAULT));
-      boardPiece.squares[0][1].add(new JLabel(BlackBlue));
+      JLabel LableBlackBlue = new JLabel(null, BlackBlue, JLabel.CENTER);
+      boardPiece.squares[0][2].add(LableBlackBlue);
       
       ImageIcon BlackLightBlue = new ImageIcon(new ImageIcon("BlackLightBlue.png").getImage().getScaledInstance(45, 45, Image.SCALE_DEFAULT));
-      boardPiece.squares[0][2].add(new JLabel(BlackLightBlue));
+      JLabel LableBlackLightBlue = new JLabel(null, BlackLightBlue, JLabel.CENTER);
+      boardPiece.squares[0][3].add(LableBlackLightBlue);
+      
       
       ImageIcon BlackPink = new ImageIcon(new ImageIcon("BlackPink.png").getImage().getScaledInstance(45, 45, Image.SCALE_DEFAULT));
-      boardPiece.squares[0][3].add(new JLabel(BlackPink));
+      JLabel LableBlackPink = new JLabel(null, BlackPink, JLabel.CENTER);
+      boardPiece.squares[0][4].add(LableBlackPink);
       
       ImageIcon BlackYellow = new ImageIcon(new ImageIcon("BlackYellow.png").getImage().getScaledInstance(45, 45, Image.SCALE_DEFAULT));
-      boardPiece.squares[0][4].add(new JLabel(BlackYellow));
+      JLabel LableBlackYellow = new JLabel(null, BlackYellow, JLabel.CENTER);
+      boardPiece.squares[0][5].add(LableBlackYellow);
       
       ImageIcon BlackRed = new ImageIcon(new ImageIcon("BlackRed.png").getImage().getScaledInstance(45, 45, Image.SCALE_DEFAULT));
-      boardPiece.squares[0][5].add(new JLabel(BlackRed));
+      JLabel LableBlackRed = new JLabel(null, BlackRed, JLabel.CENTER);
+      boardPiece.squares[0][6].add(LableBlackRed);
       
       ImageIcon BlackGreen = new ImageIcon(new ImageIcon("BlackGreen.png").getImage().getScaledInstance(45, 45, Image.SCALE_DEFAULT));
-      boardPiece.squares[0][6].add(new JLabel(BlackGreen));
+      JLabel LableBlackGreen = new JLabel(null, BlackGreen, JLabel.CENTER);
+      boardPiece.squares[0][7].add(LableBlackGreen);
       
       ImageIcon BlackBrown = new ImageIcon(new ImageIcon("BlackBrown.png").getImage().getScaledInstance(45, 45, Image.SCALE_DEFAULT));
-      boardPiece.squares[0][7].add(new JLabel(BlackBrown));
+      JLabel LableBlackBrown  = new JLabel(null, BlackBrown , JLabel.CENTER);
+      boardPiece.squares[7][0].add(LableBlackBrown);
       
       ImageIcon WhiteOrange = new ImageIcon(new ImageIcon("WhiteOrange.png").getImage().getScaledInstance(45, 45, Image.SCALE_DEFAULT));
-      boardPiece.squares[7][0].add(new JLabel(WhiteOrange));
+      JLabel LableWhiteOrange  = new JLabel(null, WhiteOrange , JLabel.CENTER);
+      boardPiece.squares[7][7].add(LableWhiteOrange);
       
       ImageIcon WhiteBlue = new ImageIcon(new ImageIcon("WhiteBlue.png").getImage().getScaledInstance(45, 45, Image.SCALE_DEFAULT));
-      boardPiece.squares[7][6].add(new JLabel(WhiteBlue));
+      JLabel LableWhiteBlue  = new JLabel(null,WhiteBlue , JLabel.CENTER);
+      boardPiece.squares[7][6].add(LableWhiteBlue);
       
       ImageIcon WhiteLightBlue = new ImageIcon(new ImageIcon("WhiteLightBlue.png").getImage().getScaledInstance(45, 45, Image.SCALE_DEFAULT));
-      boardPiece.squares[7][5].add(new JLabel(WhiteLightBlue));
+      JLabel LableWhiteLightBlue  = new JLabel(null,WhiteLightBlue, JLabel.CENTER);
+      boardPiece.squares[7][5].add(LableWhiteLightBlue);
       
       ImageIcon WhitePink = new ImageIcon(new ImageIcon("WhitePink.png").getImage().getScaledInstance(45, 45, Image.SCALE_DEFAULT));
-      boardPiece.squares[7][4].add(new JLabel(WhitePink));
-      
+      JLabel LableWhitePink  = new JLabel(null,WhitePink, JLabel.CENTER);
+      boardPiece.squares[7][4].add(LableWhitePink);
+            
       ImageIcon WhiteYellow = new ImageIcon(new ImageIcon("WhiteYellow.png").getImage().getScaledInstance(45, 45, Image.SCALE_DEFAULT));
-      boardPiece.squares[7][3].add(new JLabel(WhiteYellow));
+      JLabel LableWhiteYellow  = new JLabel(null,WhiteYellow, JLabel.CENTER);
+      boardPiece.squares[7][3].add(LableWhiteYellow);
       
       ImageIcon WhiteRed = new ImageIcon(new ImageIcon("WhiteRed.png").getImage().getScaledInstance(45, 45, Image.SCALE_DEFAULT));
-      boardPiece.squares[7][2].add(new JLabel(WhiteRed));
+      JLabel LableWhiteRed = new JLabel(null,WhiteRed, JLabel.CENTER);
+      boardPiece.squares[7][2].add(LableWhiteRed);
       
       ImageIcon WhiteGreen = new ImageIcon(new ImageIcon("WhiteGreen.png").getImage().getScaledInstance(45, 45, Image.SCALE_DEFAULT));
-      boardPiece.squares[7][1].add(new JLabel(WhiteGreen));
+      JLabel LableWhiteGreen = new JLabel(null,WhiteGreen, JLabel.CENTER);
+      boardPiece.squares[7][1].add(LableWhiteGreen);
       
       ImageIcon WhiteBrown = new ImageIcon(new ImageIcon("WhiteBrown.png").getImage().getScaledInstance(45, 45, Image.SCALE_DEFAULT));
-      boardPiece.squares[7][7].add(new JLabel(WhiteBrown));
+      JLabel LableWhiteBrown = new JLabel(null,WhiteBrown, JLabel.CENTER);
+      boardPiece.squares[0][0].add(LableWhiteBrown);
    
       frame.add(panel);
       frame.setSize(1000, 1000);
